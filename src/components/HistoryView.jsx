ae886f03def8caa808806365646c1b90d4865282
hackurity01 <hackurity01@gmail.com>
2024-11-03 16:40:36 +0900

    simple diary 구현

--- src/components/HistoryView.jsx
+++ src/components/HistoryView.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import "./HistoryView.css";
 
 function HistoryView({ setView }) {
+  const [history, setHistory] = useState({});
+
+  useEffect(() => {
+    setHistory(JSON.parse(localStorage.getItem("diary") || "{}"));
+  }, []);
+
   return (
     <>
       <div style={{ display: "flex", alignItems: "center" }}>
@@ -9,10 +16,12 @@ function HistoryView({ setView }) {
         </button>
         <h4>다이어리 기록</h4>
       </div>
-      <div className="diary-item">
-        <div className="diary-date">(날짜)</div>
-        <div>(내용)</div>
-      </div>
+      {Object.entries(history).map(([key, value]) => (
+        <div key={key} className="diary-item">
+          <div className="diary-date">{key}</div>
+          <div>{value}</div>
+        </div>
+      ))}
     </>
   );
 }
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,10 +1,39 @@
 import "./MainView.css";
+import { useState, useEffect } from "react";
 
 function MainView(props) {
+  const now = new Date();
+  const [questions, setQuestions] = useState();
+  const [answer, setAnswer] = useState("");
+  const todayKey =
+    now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/hackurity01/simple-diary/main/src/questions.json"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setQuestions(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    setAnswer(localStorage.getItem(todayKey));
+  }, []);
+
+  if (!questions) {
+    return "loading";
+  }
+
   return (
     <>
       <div className="header">
-        <div>(오늘 날짜)</div>
+        <div>
+          {now.getFullYear()}년 {now.getMonth() + 1}월 {now.getDate()}일
+        </div>
         <div>
           <button
             className="history-btn"
@@ -15,11 +44,17 @@ function MainView(props) {
           </button>
         </div>
       </div>
-      <div className="question">(질문)</div>
+      <div className="question">{questions[now.getDate()]}</div>
       <div className="content">
         <textarea
+          value={answer}
           onChange={(e) => {
-            console.log("onChange");
+            const history = JSON.parse(localStorage.getItem("diary") || "{}");
+            localStorage.setItem(
+              "diary",
+              JSON.stringify({ ...history, [todayKey]: e.target.value })
+            );
+            setAnswer(e.target.value);
           }}
         />
       </div>
